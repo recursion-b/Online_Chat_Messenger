b89c725739faaf7a1db885964be64e6c0a708201
februar-END <nobuhiro.0320@gmail.com>
2023-09-24 23:29:55 +0900

    stage1

--- client.py
+++ client.py
@@ -1,36 +1,83 @@
 import socket
 import threading
-import asyncio
-
-SERVER_ADDRESS = "localhost"
-SERVER_PORT = 9001
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-client_name = input("名前を入力してください：")
-client_socket.sendto(client_name.encode(), (SERVER_ADDRESS, SERVER_PORT))
+import time
+import sys
 
 
-def receive_messages():
-    while True:
-        message, _ = client_socket.recvfrom(4096)
-        message = message.decode("utf-8")
-        print(f"\nFROM {message}")
-        print("メッセージを入力してください：")
+class Client:
+    def __init__(self):
+        self.__SERVER_ADDRESS = "localhost"
+        self.__SERVER_PORT = 9001
+        self.__MAX_USERNAME_BYTE = 255
+        self.__TIME_LIMIT = 60
+        self.LAST_ACTIVITY_TIME = 0
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def enter_client_name(self):
+        while True:
+            client_name = input("名前を入力してください：")
+            if len(client_name.encode()) > self.__MAX_USERNAME_BYTE:
+                print("ユーザー名は255バイト以内で入力してください")
+            elif len(client_name.encode()) == 0:
+                print("ユーザー名を入力してください")
+            else:
+                self.client_socket.sendto(
+                    client_name.encode(), (self.__SERVER_ADDRESS, self.__SERVER_PORT)
+                )
+                self.LAST_ACTIVITY_TIME = time.time()
+                print("チャットルームに参加しました。")
+                break
+
+    def receive_messages(self):
+        while True:
+            message, _ = self.client_socket.recvfrom(4096)
+            message = message.decode("utf-8")
+            print(f"\nFROM {message}")
+
+    def send_messages(self):
+        while True:
+            message = input()
+            if message == "exit":
+                print("チャットルームを退出しました。")
+                break
+            else:
+                self.client_socket.sendto(
+                    message.encode("utf-8"), (self.__SERVER_ADDRESS, self.__SERVER_PORT)
+                )
+                self.LAST_ACTIVITY_TIME = time.time()
+
+    def check_timeout(self):
+        while True:
+            current_time = time.time()
+            if current_time - self.LAST_ACTIVITY_TIME > self.__TIME_LIMIT:
+                break
+            time.sleep(10)
+
+    def exit(self):
+        print("接続を切断します。")
+        self.client_socket.close()
+        sys.exit()
 
 
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.start()
+class Main:
+    client = Client()
+    clientname_thread = threading.Thread(target=client.enter_client_name)
+    clientname_thread.start()
+    clientname_thread.join()
+
+    receive_message_thread = threading.Thread(target=client.receive_messages)
+    receive_message_thread.start()
+
+    send_message_thread = threading.Thread(target=client.send_messages)
+    send_message_thread.start()
+
+    timeout_thread = threading.Thread(target=client.check_timeout)
+    timeout_thread.start()
+
+    timeout_thread.join()
+    client.exit()
 
 
-async def send_messages():
-    while True:
-        message = await asyncio.to_thread(input, "メッセージを入力してください：")
-        client_socket.sendto(message.encode("utf-8"), (SERVER_ADDRESS, SERVER_PORT))
-        print("メッセージを送信しました。")
-
-
-asyncio.run(send_messages())
-
-receive_thread.join()
-client_socket.close()
+if __name__ == "__main__":
+    Main()
--- server.py
+++ server.py
@@ -1,30 +1,41 @@
 import socket
-import threading
 
-SERVER_ADDRESS = "0.0.0.0"
-SERVER_PORT = 9001
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((SERVER_ADDRESS, SERVER_PORT))
+class Server:
+    def __init__(self):
+        self.__SERVER_ADDRESS = "0.0.0.0"
+        self.__SERVER_PORT = 9001
+        self.__MAX_MESSAGE_SIZE = 4096
+        self.clients = {}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind((self.__SERVER_ADDRESS, self.__SERVER_PORT))
 
-clients = {}
+    def standby(self):
+        print("チャットサーバーがスタートしました。")
 
-print("チャットサーバーがスタートしました。")
+        while True:
+            data, client_address = self.server_socket.recvfrom(self.__MAX_MESSAGE_SIZE)
+            message = data.decode("utf-8")
 
-while True:
-    message, client_address = server_socket.recvfrom(4096)
-    message = message.decode("utf-8")
+            if client_address not in self.clients:
+                client_name = message
+                self.clients[client_address] = client_name
+                print(f"{client_name}がチャットに参加しました")
+            else:
+                print(f"受信したメッセージ)({client_address}):{message}")
 
-    if client_address not in clients:
-        client_name = message
-        clients[client_address] = client_name
-        print(f"{client_name}がチャットに参加しました")
+                for client, _ in self.clients.items():
+                    if client != client_address:
+                        self.server_socket.sendto(
+                            f"{self.clients[client_address]}: {message}".encode(),
+                            client,
+                        )
 
-    else:
-        print(f"受信したメッセージ)({client_address}):{message}")
 
-        for client, name in clients.items():
-            if client != client_address:
-                server_socket.sendto(
-                    f"{clients[client_address]}: {message}".encode(), client
-                )
+class Main:
+    server = Server()
+    server.standby()
+
+
+if __name__ == "__main__":
+    Main()
