5cf402f921b2a50a1b0e8c02619e37663da60625
AkinoJoey <gakiya213@gmail.com>
2023-09-26 16:49:47 +0900

    bind後、is_server_alive()でサーバーが起動しているか確認できるようにした

--- stage1/client.py
+++ stage1/client.py
@@ -8,10 +8,40 @@ class Client:
         self.server_address = '127.0.0.1'
         self.server_port = 8000  
         self.address = ''
-        self.port = 9001
+        self.port = 9000
+        self.max_buffer = 4096
         self.sock.bind((self.address, self.port))
         
-        self.startChat()
+        server_alive = self.is_server_alive()
+        
+        if server_alive:
+            # サーバーが起動しているのでsettimeoutをリセットする
+            self.sock.settimeout(None)
+            self.startChat()
+        else:
+            print('サーバーと接続できませんでした。')
+            self.sock.close()
+        
+    def is_server_alive(self):
+        print('サーバーとの接続を開始します...')
+        try:
+            message = 'ping'
+            packet = self.protocol_sending(message)
+            self.sock.sendto(packet,(self.server_address,self.server_port))
+
+            timeout = 1
+            self.sock.settimeout(timeout)
+            data, address = self.sock.recvfrom(self.max_buffer)
+            decoded_data = self.protocol_receiving(data)
+            message = decoded_data[1]
+            
+            if message == 'ping':
+                return True
+            else:
+                return False
+            
+        except OSError:
+            return False
         
     def startChat(self):
         self.set_username()
@@ -21,6 +51,23 @@ class Client:
         send_thread.start()
         receive_thread.start()
     
+    def protocol_sending(self, message):
+        usernamelen = len(self.username.encode())
+            
+        # packetが4096を超えていないかチェックするバリデーターが必要？
+        packet = bytes([usernamelen]) + self.username.encode() + message.encode()
+        
+        return packet
+    
+    # (username, message)のtupleを返す
+    def protocol_receiving(self, data):
+        usernamelen = data[0]
+        username_data = data[1:1 + usernamelen]
+        username = username_data.decode()
+        message = data[1 + usernamelen: ].decode()
+            
+        return (username, message)
+    
     def send(self):
         print('\nチャットを開始します。')
         
@@ -29,6 +76,7 @@ class Client:
             packet =  self.protocol_sending(message)
             
             self.sock.sendto(packet, (self.server_address, self.server_port))
+        
     
     def set_username(self):
         max_username_byte = 255
@@ -44,28 +92,15 @@ class Client:
             else:
                 self.username = username
                 break
-            
-    def protocol_sending(self, message):
-        usernamelen = len(self.username.encode())
-            
-        # packetが4096を超えていないかチェックするバリデーターが必要？
-        packet = bytes([usernamelen]) + self.username.encode() + message.encode()
-        
-        return packet
-            
+    
     def receive(self):
         while True:
-            max_buffer = 4096
-            data, address = self.sock.recvfrom(max_buffer)
-            usernamelen = data[0]
-            
-            username_data = data[1:1 + usernamelen]
-            username = username_data.decode()
-            
-            message = data[1 + usernamelen: ].decode()
+            data, address = self.sock.recvfrom(self.max_buffer)
+            decoded_data = self.protocol_receiving(data)
+            username = decoded_data[0]
+            message = decoded_data[1]
             
             print(f'\n{username}: {message}')
-        
 
 class Main:
     client = Client()
