45814f33c6aa3a1461c6df397ea1d1c147a95722
AkinoJoey <gakiya213@gmail.com>
2023-09-27 19:06:06 +0900

    tcp接続の送受信まで完了

--- stage1/client.py
+++ stage1/client.py
@@ -11,16 +11,18 @@ class Client:
         self.address = ''
         self.port = 9000
         self.max_buffer = 4096
+        self.set_username()
         
         server_alive = self.is_server_alive()
         
         if server_alive:
             # サーバーが起動しているのでsettimeoutをリセットする
             self.sock.settimeout(None)
+            
+            # tcp接続で新しいチャットルームを作成するか、既存のチャットルームに参加するか決める
             self.tcp_sock.connect((self.server_address, self.server_port))
-            self.set_username()
-            # self.choice_menu()
-            self.startChat()
+            self.choice_menu()
+            # self.startChat()
         else:
             print('サーバーと接続できませんでした。')
             self.sock.close()
@@ -55,7 +57,7 @@ class Client:
     def protocol_sending(self, message):
         usernamelen = len(self.username.encode())
             
-        # packetが4096を超えていないかチェックするバリデーターが必要？
+        # packetが4096を超えていないかチェックするバリデーターが必要
         packet = bytes([usernamelen]) + self.username.encode() + message.encode()
         
         return packet
@@ -83,7 +85,7 @@ class Client:
         max_username_byte = 255
         
         while True:
-            username = input('\nユーザー名を入力してください。\n\nusername: ')
+            username = input('\nユーザー名を入力してください。\n\nUser Name: ')
             
             if len(username.encode()) > max_username_byte:
                 print('ユーザー名の最大文字数を超えています。\n')
@@ -106,30 +108,80 @@ class Client:
     def choice_menu(self):
         while True:
             print('\n希望するメニューを入力してEnterを押してください。')
-            print('\n1:新しいチャットルームを作成する。2:既存のチャットルームに参加する')
+            print('\n1:新しいチャットルームを作成する\n2:既存のチャットルームに参加する')
             choice = input('\n-> ')
 
             if choice == '1':
                 print('新しいチャットルームを作成します。作成したいチャットルームの名前を入力してください。')
-                room_name = input('Chat Room Name: ')
-                room_name_bytes = room_name.encode()
-                username_bytes = self.username.encode()
+                room_name = input('Room Name: ')
+                self.send_menu_selection(choice, room_name)
                 
-                header = self.tcrp_header(len(room_name_bytes), 1, 0, len(username_bytes))
+                # サーバーからのリクエスト応答を受け取る
+                self.receive_response()
                 
-                self.tcp_sock.sendall(header)
-                self.tcp_sock.sendall(room_name_bytes + username_bytes)
+                # サーバーからのリクエスト完了を受け取る
+                self.receive_response()
+                
+                print('チャットルームの作成が完了しました。')
+                
+                self.tcp_sock.close()
+                
                 break
             elif choice == '2':
                 print('参加したいチャットルームの名前を入力してください。')
                 room_name = input('Chat Room Name: ')
+                self.send_menu_selection(choice, room_name)
+                
+                # サーバーからのリクエスト応答を受け取る
+                self.receive_response()
+                
+                # サーバーからのリクエスト完了を受け取る
+                self.receive_response()
+                
+                print(f'{room_name}に参加しました。')
+                
+                self.tcp_sock.close()
+                
                 break
             else:
                 print('\n1または2を選択してください。')
                 
+    
+    def send_menu_selection(self, choice, room_name):
+        room_name_bytes = room_name.encode()
+        operation_payload_bytes = self.username.encode()
+
+        # 255バイト以下かどうか確認するバリデータが必要
+        room_name_size = len(room_name_bytes)
+        operation_payload_size = len(operation_payload_bytes)
+        # リクエストのstateコードは0
+        state = 0
+        header = self.tcrp_header(room_name_size,int(choice),state, operation_payload_size)
+        
+        self.tcp_sock.sendall(header)
+        self.tcp_sock.sendall(room_name_bytes + operation_payload_bytes)
+    
+    def receive_response(self):
+        header_size = 32
+        header = self.tcp_sock.recv(header_size)
+        room_name_size = header[0]
+        operation = header[1]
+        state = header[2]
+        operation_payload_size = int.from_bytes(header[3:])
+        
+        response_body = self.tcp_sock.recv(room_name_size + operation_payload_size)
+        room_name = response_body[0: room_name_size]
+        payload = response_body[room_name_size:]
+        print(f'\nRoom Name: {room_name.decode()}')
+        print(f'Operation: {operation}')
+        print(f'State: {state}')
+        print(f'Payload: {payload.decode()}')
         
     def tcrp_header(self,room_name_size, operation, state ,operation_payload_size):
-            return room_name_size.to_bytes(1, 'big') + operation.to_bytes(1, 'big') + state.to_bytes(1, 'big') + operation_payload_size.to_bytes(29, 'big')
+        return room_name_size.to_bytes(1, 'big') + operation.to_bytes(1, 'big') + state.to_bytes(1, 'big') + operation_payload_size.to_bytes(29, 'big')
+    
+    
+    
         
 
 class Main:
--- stage1/server.py
+++ stage1/server.py
@@ -1,12 +1,14 @@
 import socket
 import datetime
+import uuid
 
 class Server:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.address = '127.0.0.1'
         self.port = 8000
-        self.clientMap = {} # {'address':{'address':address, 'last_message_time':datetime.datetime.now()}}
+        self.client_map = {} # {'address':{'address':address, 'last_message_time':datetime.datetime.now()}}
+        self.host_map = {} # {unique token: {address: address, username: username}}
         self.sock.bind((self.address, self.port))
         
         print('サーバーを起動しました。')
@@ -18,48 +20,115 @@ class Server:
         
         while True:
             data, address = self.sock.recvfrom(max_buffer)
-            usernamelen = data[0]
+            decoded_data = self.protocol_receiving(data)
+            username = decoded_data[0]
+            message = decoded_data[1]
             
-            username_data = data[1:1 + usernamelen]
-            username = username_data.decode()
-            
-            message = data[1 + usernamelen: ].decode()
-            
-            self.update_last_message(address)    
-            
+            if address in self.client_map:
+                self.update_last_message(address)
+                
+            else:
+                self.add_client_map(address)
+                
             print(f'{username}: {message}')
-
             if data:
                 packet = self.protocol_sending(username, message)
                 self.send(packet)
+            
+            # tcp接続を開始
+            tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # 同じポートをすぐに再利用するためのオプション
+            tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            tcp_sock.bind((self.address, self.port))
+            tcp_sock.listen(5)
+            client_socket, address = tcp_sock.accept()
+            
+            # リクエストの内容を受信
+            request = self.receive_request(client_socket)
+            room_name_size = request[0]
+            operation = request[1]
+            room_name_bytes = request[2]
+            
+            # リクエスト応答のレスポンス
+            response_state = 1
+            accepted_status_code = '202 Accepted'
+            self.send_response(client_socket, room_name_size, room_name_bytes, operation, response_state, accepted_status_code)
+            
+            # リクエスト完了のレスポンス
+            done_state = 2 
+            unique_token = str(uuid.uuid4()) # 16バイトのトークン
+            self.send_response(client_socket, room_name_size, room_name_bytes, operation, done_state, unique_token)
+            self.add_host_map(unique_token, address, username)
+            
+            # 新しいチャットルームに参加、あるいは既存のチャットルームに参加させる挙動をここに追加
+            
+            client_socket.close()
+            tcp_sock.close()
+            
     
     def protocol_sending(self,username, message):
         usernamelen = len(username.encode())
         return bytes([usernamelen]) + username.encode() + message.encode()
+    
+    def protocol_receiving(self, data):
+        usernamelen = data[0]
+        username_data = data[1:1 + usernamelen]
+        username = username_data.decode()
+        message = data[1 + usernamelen: ].decode()
+            
+        return (username, message)
+    
+    def tcrp_header(self,room_name_size, operation, state ,operation_payload_size):
+        return room_name_size.to_bytes(1, 'big') + operation.to_bytes(1, 'big') + state.to_bytes(1, 'big') + operation_payload_size.to_bytes(29, 'big')
+
+    def receive_request(self,client_socket):
+        header_size = 32
+        header = client_socket.recv(header_size)
+        room_name_size = header[0]
+        operation = header[1]
+        state = header[2]
+        operation_payload_size = int.from_bytes(header[3:])
+        
+        # クライアントからデータを受信
+        data = client_socket.recv(room_name_size + operation_payload_size)
+        room_name_bytes = data[0:room_name_size]
+        operation_payload = data[room_name_size:]
+        
+        print(f'\nRoom Name: {room_name_bytes.decode()}')
+        print(f'Operation: {operation}')
+        print(f'State: {state}')
+        print(f'Payload: {operation_payload.decode()}')
+        
+        return (room_name_size, operation, room_name_bytes)
+    
+    def send_response(self, client_socket ,room_name_size, room_name_bytes ,operation, state, operation_payload):
+        header = self.tcrp_header(room_name_size, operation, state, len(operation_payload.encode()))
+        client_socket.sendall(header)
+        client_socket.sendall(room_name_bytes + operation_payload.encode())
             
     def send(self, packet):
-            for address in self.clientMap:
+            for address in self.client_map:
                 time_over = self.check_if_time_over(address)
                 
                 if time_over:
-                    self.delete_address_from_clientMap(address)
+                    self.delete_address_from_client_map(address)
                 else:
                     self.sock.sendto(packet, address)     
     
     def check_if_time_over(self, address):
         # 現在の時刻と最後のメッセージの送信時刻を比較する
-        td = datetime.datetime.now() - self.clientMap[address]['last_message_time']
+        td = datetime.datetime.now() - self.client_map[address]['last_message_time']
         
         # 時間、分、秒に変換
         h, m, s = self.get_h_m_s(td)
         
         # 最後のメッセージが30分以上経過していたらTrueを返す
         if m > 30:
-            self.clientMap.pop(address)
+            self.client_map.pop(address)
             return True
     
-    def delete_address_from_clientMap(self, address):
-        self.clientMap.pop(address)
+    def delete_address_from_client_map(self, address):
+        self.client_map.pop(address)
                 
     def get_h_m_s(self, td):
         m, s = divmod(td.seconds, 60)
@@ -67,11 +136,14 @@ class Server:
         return h, m, s
 
     def update_last_message(self, address):
-        if self.clientMap in address:
-            self.clientMap[address]['last_message_time'] = datetime.datetime.now()    
-        else:
-            self.clientMap[address] = {'address':address, 'last_message_time':datetime.datetime.now()}
-            
+        self.client_map[address]['last_message_time'] = datetime.datetime.now()    
+        
+    def add_client_map(self, address):
+        self.client_map[address] = {'address':address, 'last_message_time':datetime.datetime.now()}
+        
+    def add_host_map(self,unique_token, address, username):
+        self.client_map[unique_token] = {'address': address, 'username': username}
+        
 class Main:
     server = Server()
     server.receive()
