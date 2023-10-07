0a9a1f776204a2c57c80d22902ca35cdc27a2f5f
AkinoJoey <gakiya213@gmail.com>
2023-10-08 00:00:52 +0900

    チャットルームクラスを作成

--- stage3/src/server.js
+++ stage3/src/server.js
@@ -10,27 +10,93 @@ const server = http.createServer(app);
 const socketIo = io(server);
 
 class ClientInfo {
-    constructor(socket, userName = null) {
+    constructor(socket, userName, access_token, is_host) {
         this.socket = socket;
         this.userName = userName;
-        this.access_token = null;
+        this.access_token = access_token;
         this.connected_at = Date.now();
-        this.is_host = false;
+        this.is_host = is_host;
         this.last_message_time = Date.now();
     }
 
-    generateToken() {
-        this.access_token = crypto.randomBytes(16).toString('hex');
-        return this.access_token;
+}
+
+
+class ChatRoom{
+    constructor(roomName){
+        this.roomName = roomName;
+        this.clientInfos = [];
+    }
+
+    addClientInfo(clientInfo) {
+        this.clientInfos.push(clientInfo);
+    }
+
+    broadcastMessageToClients(token, message, userName){
+        let roomMessage = { content: `${userName} -> ${message}`, token: token }; // ここを変更
+        for (const client of this.clientInfos) {
+            client.socket.emit('message', roomMessage);
+        }
+    }
+
+    broadcastRoomInfo() {
+        const clientInfo = this.clientInfos.map(client =>{
+            const info = {
+                access_token: client.access_token,
+                last_message_time: client.connected_at
+            };
+
+            if (client.is_host) {
+                info.host_name = client.userName;
+            }
+            console.log(info)
+            return info;
+        });
+        console.log(`Sending updateRoomInfo event for room: ${this.roomName}`);
+        for (const client of this.clientInfos) {
+            client.socket.emit('updateRoomInfo', this.roomName, clientInfo);
+        }
+    }
+
+    findInActiveClients(){
+        let clientsToRemove = []
+
+        for(const clientInfo of this.clientInfos){
+            const inactivityThreshold = 30 * 1000;
+            if(Date.now() - clientInfo.last_message_time > inactivityThreshold);
+
+            clientsToRemove.push(clientInfo)
+
+            if(clientInfo.is_host){
+                clientsToRemove.push(
+                    ...this.clientInfos.filter((info) => !clientsToRemove.includes(info))
+                );
+                break
+            }
+        }
+
+        return clientsToRemove
+    }
+
+    removeClients(clientsToRemove){
+        for(const clientInfo of clientsToRemove){
+            let index = this.clientInfos.indexOf(clientInfo);
+            if (index !== -1) {
+                this.clientInfos.splice(index, 1);
+            }
+        }
     }
 }
 
-let chatRooms = {};
+
+
+let chatRooms = {}; // {roomName: ClientInfo obj} -> {roomName: ChatRoom obj}
+let tokens = {}; // {token: room_name}
+let clients = {}; // {token: client_info}
 
 socketIo.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    const client = new ClientInfo(socket);
 
     socket.on('createRoom', (userName, roomName, callback) => {
         if (typeof roomName !== 'string' || roomName.trim() === '') {
@@ -38,13 +104,22 @@ socketIo.on('connection', (socket) => {
         }
         console.log("部屋作成！！")
         if (!chatRooms[roomName]) {
-            chatRooms[roomName] = [];
-            client.userName = userName;
-            client.is_host = true;
-            const token = client.generateToken();
-            chatRooms[roomName].push(client);
+            // tokenの作成
+            const token = generateToken();
+
+            // チャットルームの作成
+            const chatRoom = new ChatRoom(roomName);
+            chatRooms[roomName] = chatRoom            
+            tokens[token] = roomName
+
+            // clientInfoの作成
+            const client = new ClientInfo(socket, userName, token, true);
+            clients[token] = client
+            
+            chatRoom.addClientInfo(client)
+
             callback({ token, clientInfo: { userName: client.userName, access_token: client.access_token, is_host: client.is_host } });
-            broadcastRoomInfo(roomName);
+            chatRoom.broadcastRoomInfo(roomName);
         } else {
             callback({ error: "Room already exists" });
         }
@@ -56,11 +131,21 @@ socketIo.on('connection', (socket) => {
         }
         console.log("部屋参加！！")
         if (chatRooms[roomName]) {
-            client.userName = userName;
-            const token = client.generateToken();
-            chatRooms[roomName].push(client);
+            // tokenの作成
+            const token = generateToken();
+
+            // チャットルームの作成
+            const chatRoom = chatRooms[roomName]
+            tokens[token] = roomName
+
+            // clientInfoの作成
+            const client = new ClientInfo(socket, userName, token, true);
+            clients[token] = client
+
+            chatRoom.addClientInfo(client)
+
             callback({ token, clientInfo: { userName: client.userName, access_token: client.access_token, is_host: client.is_host } });
-            broadcastRoomInfo(roomName);
+            chatRoom.broadcastRoomInfo(roomName);
         } else {
             callback({ error: "Room doesn't exist" });
         }
@@ -68,12 +153,11 @@ socketIo.on('connection', (socket) => {
 
     socket.on('message', (token, message, userName) => {
         console.log(message);
-        const roomName = Object.keys(chatRooms).find(room => chatRooms[room].some(c => c.access_token === token));
-        if (roomName) {
-            let roomMessage = { content: `${userName} -> ${message}`, token: token }; // ここを変更
-            for (const client of chatRooms[roomName]) {
-                client.socket.emit('message', roomMessage);
-            }
+        const roomName = tokens[token]
+        const chatRoom = chatRooms[roomName];
+
+        if (chatRoom) {
+            chatRoom.broadcastMessageToClients(token, message, userName)
         }
     });
 
@@ -88,53 +172,84 @@ socketIo.on('connection', (socket) => {
         console.log('User disconnected:', socket.id);
     });
 
-    function broadcastRoomInfo(roomName) {
-        const clientsInfo = chatRooms[roomName].map(client => {
-            const info = {
-                access_token: client.access_token,
-                last_message_time: client.connected_at
-            };
+    // function broadcastRoomInfo(roomName) {
+    //     const clientsInfo = chatRooms[roomName].map(client => {
+    //         const info = {
+    //             access_token: client.access_token,
+    //             last_message_time: client.connected_at
+    //         };
 
-            if (client.is_host) {
-                info.host_name = client.userName;
+    //         if (client.is_host) {
+    //             info.host_name = client.userName;
+    //         }
+    //         console.log(info)
+    //         return info;
+    //     });
+    //     console.log(`Sending updateRoomInfo event for room: ${roomName}`);
+    //     for (const client of chatRooms[roomName]) {
+    //         client.socket.emit('updateRoomInfo', roomName, clientsInfo);
+    //     }
+    // }
+
+    function generateToken(){
+        return crypto.randomBytes(16).toString('hex');
+    }
+
+    
+
+    function checkForInactiveClients() {
+
+        for(const chatRoom of Object.values(chatRooms)){
+            clientsToRemove = chatRoom.findInActiveClients();
+            removeTokensAndClients(clientsToRemove);
+            chatRoom.removeClients(clientsToRemove);
+            deleteRoomIfEmpty(chatRoom);
+        }
+
+        // for (const room in chatRooms) {
+        //     const clientsToRemove = [];
+
+        //     for (const client of chatRooms[room]) {
+        //         if (Date.now() - client.last_message_time > inactivityThreshold) {
+        //             clientsToRemove.push(client);
+        //         }
+        //     }
+
+        //     for (const inactiveClient of clientsToRemove) {
+        //         chatRooms[room] = chatRooms[room].filter(c => c !== inactiveClient);
+        //         inactiveClient.socket.emit('message', `[Server]: ${inactiveClient.userName} has been disconnected due to inactivity.`);
+        //         inactiveClient.socket.disconnect();
+        //     }
+
+        //     if (chatRooms[room].length === 0) {
+        //         delete chatRooms[room];
+        //     } else if (chatRooms[room]) { 
+        //         broadcastRoomInfo(room);
+        //     }
+        // }
+    }
+
+    function removeTokensAndClients(clientsToRemove){
+        for(const clientInfo of clientsToRemove){
+            if (clientInfo.access_token in tokens){
+                delete tokens[clientInfo.access_token]
             }
-            console.log(info)
-            return info;
-        });
-        console.log(`Sending updateRoomInfo event for room: ${roomName}`);
-        for (const client of chatRooms[roomName]) {
-            client.socket.emit('updateRoomInfo', roomName, clientsInfo);
+            if(clientInfo.access_token in clients){
+                delete clients[clientInfo.access_token]
+            }
+        }
+    }
+    
+    function deleteRoomIfEmpty(chatRoom){
+        if(chatRoom.clientInfos.length == 0){
+            delete chatRooms[chatRoom.roomName]
         }
     }
 
-//     function checkForInactiveClients() {
-//         const inactivityThreshold = 30 * 1000; 
-
-//         for (const room in chatRooms) {
-//             const clientsToRemove = [];
-
-//             for (const client of chatRooms[room]) {
-//                 if (Date.now() - client.last_message_time > inactivityThreshold) {
-//                     clientsToRemove.push(client);
-//                 }
-//             }
-
-//             for (const inactiveClient of clientsToRemove) {
-//                 chatRooms[room] = chatRooms[room].filter(c => c !== inactiveClient);
-//                 inactiveClient.socket.emit('message', `[Server]: ${inactiveClient.userName} has been disconnected due to inactivity.`);
-//                 inactiveClient.socket.disconnect();
-//             }
-
-//             if (chatRooms[room].length === 0) {
-//                 delete chatRooms[room];
-//             } else if (chatRooms[room]) { 
-//                 broadcastRoomInfo(room);
-//             }
-//         }
-//     }
-
-//     setInterval(checkForInactiveClients, 10 * 1000);
+    setInterval(checkForInactiveClients, 10 * 1000);
 });
+
+
 
 // Reactのフロントエンドを提供するための設定
 if (process.env.NODE_ENV === 'production') {
